ad783329f779d9217653d9453beb20eb4526e8e3
sumaiyakhn1 <sumaiyakn28@gmail.com>
2024-10-20 21:27:05 +0530

    AboutUni

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import SliderImage from "../components/Home/CarouselComponents"
 import Highlights from "../components/Home/Highlights"
 import PopularCourse from '../components/Home/PopularCourse'
+import AboutUni from '../components/Home/AboutUni'
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
     <SliderImage/>
     <Highlights/>
     <PopularCourse/>
+    <AboutUni/>
     
 
 
