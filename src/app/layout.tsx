0ad4a6c41776bb0685014aaa3a3d9a51082e465e
Sundram <118174959+rajputsundram@users.noreply.github.com>
2025-01-09 15:27:48 +0530

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <ThemeProviderWrapper attribute="class">
+        <ThemeProviderWrapper>
       
         <Header/>
      
